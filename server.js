0acedccd34288c52dcb63a3e8467e7fc6a7b9533
Goat <dpajbk@gmail.com>
2022-11-01 17:00:55 -0400

    added dependencies, npm init

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const figlet = require('figlet');
+const consoleTable = require('console.table');
