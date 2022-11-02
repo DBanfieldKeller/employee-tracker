e4fb24aecae1dba37eb6e5713f8cdcb2cfe1f04c
Goat <dpajbk@gmail.com>
2022-11-01 23:16:17 -0400

    add Employee function added

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const figlet = require('figlet');
 const cTable = require('console.table');
-const { sample } = require('rxjs');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
@@ -25,6 +23,9 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employees_db database.`)
 );
+
+// inquirer functions
+
 
 
 // query functions
@@ -89,11 +90,88 @@ const viewEmployees = () => {
 
 // add an employee
 
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: "What is the employee's first name?",
+            validate: addFirstName => {
+                if (addFirstName) {
+                    return true;
+                } else {
+                    console.log('Please enter a first name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: "What is the employee's last name?",
+            validate: addLastName => {
+                if (addLastName) {
+                    return true;
+                } else {
+                    console.log('Please enter a last name');
+                    return false;
+                }
+            }
+        }
+    ])
+        .then(answer => {
+            const crit = [answer.firstName, answer.lastName]
+            const roleSql = `SELECT r.id, r.title FROM roles r`;
+            db.query(roleSql, (err, data) => {
+                if (err) throw err;
+                const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: "Employee's role?",
+                        choices: roles
+                    }
+                ])
+                    .then(roleChoice => {
+                        const role = roleChoice.role;
+                        crit.push(role);
+                        const managerSql = `SELECT * FROM employee`;
+                        db.query(managerSql, (error, data) => {
+                            if (error) throw error;
+                            const managers = data.map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id }));
+                            inquirer.prompt([
+                                {
+                                    type: 'list',
+                                    name: 'manager',
+                                    message: "Employee's manager?",
+                                    choices: managers
+                                }
+                            ])
+                                .then(managerChoice => {
+                                    const manager = managerChoice.manager;
+                                    crit.push(manager);
+                                    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                                    VALUES (?, ?, ?, ?)`;
+                                    db.query(sql, crit, (error) => {
+                                        if (error) throw error;
+                                        console.log("Employee has been added!")
+                                        viewEmployees();
+                                    });
+                                });
+                        });
+                    });
+            });
+        });
+};
+
+
 // update an employee role
 
-viewDepartments();
-viewEmployees();
-viewRoles();
+// viewDepartments();
+// viewEmployees();
+// viewRoles();
+addEmployee();
 
 
 // Default response for any other request (Not Found)
