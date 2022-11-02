fcb8fe2663d006d05ef7118dbb6f0e89e9dd8f83
Goat <dpajbk@gmail.com>
2022-11-01 21:06:19 -0400

    connection to db added

--- server.js
+++ server.js
@@ -4,3 +4,33 @@ const mysql = require('mysql2');
 const figlet = require('figlet');
 const consoleTable = require('console.table');
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: process.env.PASSWORD,
+        database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
